31dd43827489d8456107574a62591dcf5ac28f6b
Aditi Verma <aditiverma1009@gmail.com>
2021-03-08 01:55:31 +0530

    check on cart count increase lift up state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Cart from "./components/Cart/Cart";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AllOrders from "./components/AllOrders/AllOrders";
+import axios from "axios";
+import BASE_URL from "./constants/index.js";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header cartValue={"1"} />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/all-orders">
-            <AllOrders />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      products: [],
+      cartCount: 0,
+    };
+  }
+
+  componentDidMount = async () => {
+    const { data, error } = await axios.get(`${BASE_URL}/items`);
+    const products = data.data;
+    if (products) {
+      this.setState({
+        isLoaded: true,
+        products: products.map((eachProduct) => {
+          return {
+            ...eachProduct,
+            inCartCount: 0,
+          };
+        }),
+      });
+    } else if (error) {
+      this.setState({
+        isLoaded: true,
+        error: error,
+      });
+    }
+  };
+
+  onIncrement(id) {
+    const product = this.state.products.find(
+      (eachProduct) => eachProduct.id === id
+    );
+    if (product && product.count > product.inCartCount && product.count !== 0) {
+      const newState = {
+        ...this.state,
+        cartCount: this.state.cartCount + 1,
+        products: this.state.products.map((eachProduct) => {
+          if (eachProduct.id === id) {
+            return { ...eachProduct, inCartCount: eachProduct.inCartCount + 1 };
+          }
+          return eachProduct;
+        }),
+      };
+      this.setState(newState);
+    }
+  }
+
+  onDecrement(id) {
+    const product = this.state.products.find(
+      (eachProduct) => eachProduct.id === id
+    );
+    if (product && product.inCartCount > 0) {
+      const newState = {
+        ...this.state,
+        cartCount: this.state.cartCount - 1,
+        products: this.state.products.map((eachProduct) => {
+          if (eachProduct.id === id) {
+            return { ...eachProduct, inCartCount: eachProduct.inCartCount - 1 };
+          } else {
+            return eachProduct;
+          }
+        }),
+      };
+      this.setState(newState);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Header cartValue={this.state.cartCount} />
+          <Switch>
+            <Route path="/" exact>
+              <Home
+                products={this.state.products}
+                onIncrement={(id) => this.onIncrement(id)}
+                onDecrement={(id) => this.onDecrement(id)}
+              />
+            </Route>
+            <Route path="/all-orders">
+              <AllOrders />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,83 +1,22 @@
 import React, { Component } from "react";
 import Product from "../Product/Product";
-import axios from "axios";
-import BASE_URL from "../../constants/index";
+
 import "./Home.css";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      products: [],
-      cartCount: 0,
-    };
-  }
-
-  componentDidMount = async () => {
-    const { data, error } = await axios.get(`${BASE_URL}/items`);
-    const products = data.data;
-    if (products) {
-      this.setState({
-        isLoaded: true,
-        products: products.map((eachProduct) => {
-          return {
-            ...eachProduct,
-            inCartCount: 0,
-          };
-        }),
-      });
-    } else if (error) {
-      this.setState({
-        isLoaded: true,
-        error: error,
-      });
-    }
-  };
-
-  onIncrement(id) {
-    const newState = {
-      ...this.state,
-      cartCount: this.state.cartCount + 1,
-      products: this.state.products.map((eachProduct) => {
-        if (
-          eachProduct.id === id &&
-          eachProduct.count > eachProduct.inCartCount &&
-          eachProduct.count !== 0
-        ) {
-          return { ...eachProduct, inCartCount: eachProduct.inCartCount + 1 };
-        }
-        return eachProduct;
-      }),
-    };
-    this.setState(newState);
-  }
-
-  onDecrement(id) {
-    const newState = {
-      ...this.state,
-      cartCount: this.state.cartCount - 1,
-      products: this.state.products.map((eachProduct) => {
-        if (eachProduct.id === id && eachProduct.inCartCount > 0) {
-          return { ...eachProduct, inCartCount: eachProduct.inCartCount - 1 };
-        } else {
-          return eachProduct;
-        }
-      }),
-    };
-    this.setState(newState);
   }
 
   render() {
-    console.log(this.state.products);
-    const allProducts = this.state.products.map((eachProduct) => {
+    console.log(this.props.products);
+    const allProducts = this.props.products.map((eachProduct) => {
       return (
         <Product
           key={eachProduct.id}
           product={eachProduct}
-          onIncrement={() => this.onIncrement(eachProduct.id)}
-          onDecrement={() => this.onDecrement(eachProduct.id)}
+          onIncrement={() => this.props.onIncrement(eachProduct.id)}
+          onDecrement={() => this.props.onDecrement(eachProduct.id)}
         />
       );
     });
