5e0a0447be9598227c7bc988cd8797f998139015
Aditi Verma <aditiverma1009@gmail.com>
2021-03-09 23:12:18 +0530

    convert all order, cart to functional component

--- src/components/AllOrders/AllOrders.jsx
+++ src/components/AllOrders/AllOrders.jsx
@@ -1,71 +1,61 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./AllOrders.scss";
 
-export default class AllOrders extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      orders: [],
-    };
-  }
+const AllOrders = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [orders, setOrders] = useState([]);
 
-  componentDidMount = async () => {
+  useEffect(async () => {
     const { data, error } = await axios.get(`/orders`);
     const orders = data.data;
     if (orders) {
-      this.setState({
-        isLoaded: true,
-        orders: orders,
-      });
+      setIsLoaded(true);
+      setOrders(orders);
     } else if (error) {
-      this.setState({
-        isLoaded: true,
-        error: error,
-      });
+      setIsLoaded(true);
+      setError(error);
     }
-  };
+  }, []);
 
-  render() {
-    const { error, isLoaded, orders } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    }
-    return (
-      <div className="all-products">
-        <table className="all-products-table">
-          <tr className="order-rows">
-            <td>Order ID</td>
-            <td>Date</td>
-            <td>Items</td>
-          </tr>
-          {orders.map((eachOrder) => {
-            return (
-              <tr className="order-rows" key={eachOrder.id}>
-                <td>{eachOrder.id}</td>
-                <td>{new Date(eachOrder.date).toString()}</td>
-                <td>
-                  {eachOrder.items.map((eachItem) => {
-                    return (
-                      <table className="item-table" key={eachOrder + eachItem}>
-                        <tr>
-                          <td>{eachItem.id}</td>
-                          <td>{eachItem.name}</td>
-                          <td>{eachItem.count}</td>
-                        </tr>
-                      </table>
-                    );
-                  })}
-                </td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
-    );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
   }
-}
+  return (
+    <div className="all-products">
+      <table className="all-products-table">
+        <tr className="order-rows">
+          <td>Order ID</td>
+          <td>Date</td>
+          <td>Items</td>
+        </tr>
+        {orders.map((eachOrder) => {
+          return (
+            <tr className="order-rows" key={eachOrder.id}>
+              <td>{eachOrder.id}</td>
+              <td>{new Date(eachOrder.date).toString()}</td>
+              <td>
+                {eachOrder.items.map((eachItem) => {
+                  return (
+                    <table className="item-table" key={eachOrder + eachItem}>
+                      <tr>
+                        <td>{eachItem.id}</td>
+                        <td>{eachItem.name}</td>
+                        <td>{eachItem.count}</td>
+                      </tr>
+                    </table>
+                  );
+                })}
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+
+export default AllOrders;
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./Cart.scss";
 import axios from "axios";
-export default class Cart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-    };
-  }
 
-  componentDidMount = () => {
+const Cart = (props) => {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
     let total = 0;
-    this.props.cart.forEach((eachCartItem) => {
+    props.cart.forEach((eachCartItem) => {
       total += eachCartItem.inCartCount * eachCartItem.price;
     });
-    this.setState({
-      total,
-    });
-  };
+    setTotal(total);
+  }, []);
 
-  createOrder = () => {
+  const createOrder = () => {
     axios.post(
       "/orders",
       {
-        items: this.props.cart.map((eachCartItem) => {
+        items: props.cart.map((eachCartItem) => {
           return {
             id: eachCartItem.id,
             name: eachCartItem.name,
@@ -41,36 +35,36 @@ export default class Cart extends Component {
     );
   };
 
-  render() {
-    if (this.props.cart.length > 0) {
-      return (
-        <div className="cart-page">
-          <table className="cart-table">
-            <tr>
-              <td>Item</td>
-              <td>Unit Price</td>
-              <td>Quatity</td>
-              <td>Sub Total</td>
-            </tr>
-            {this.props.cart.map((eachCartItem) => {
-              return (
-                <tr key={eachCartItem.id}>
-                  <td>{eachCartItem.name}</td>
-                  <td>Rs. {eachCartItem.price}</td>
-                  <td>{eachCartItem.inCartCount}</td>
-                  <td>{eachCartItem.inCartCount * eachCartItem.price}</td>
-                </tr>
-              );
-            })}
-          </table>
-          <div className="total-value">
-            <div>total: {this.state.total}</div>
-            <button onClick={() => this.createOrder()}>Checkout</button>
-          </div>
+  if (props.cart.length > 0) {
+    return (
+      <div className="cart-page">
+        <table className="cart-table">
+          <tr>
+            <td>Item</td>
+            <td>Unit Price</td>
+            <td>Quatity</td>
+            <td>Sub Total</td>
+          </tr>
+          {props.cart.map((eachCartItem) => {
+            return (
+              <tr key={eachCartItem.id}>
+                <td>{eachCartItem.name}</td>
+                <td>Rs. {eachCartItem.price}</td>
+                <td>{eachCartItem.inCartCount}</td>
+                <td>{eachCartItem.inCartCount * eachCartItem.price}</td>
+              </tr>
+            );
+          })}
+        </table>
+        <div className="total-value">
+          <div>total: {total}</div>
+          <button onClick={() => createOrder()}>Checkout</button>
         </div>
-      );
-    } else {
-      return <div className="cart-page">No Items in the cart</div>;
-    }
+      </div>
+    );
+  } else {
+    return <div className="cart-page">No Items in the cart</div>;
   }
-}
+};
+
+export default Cart;
