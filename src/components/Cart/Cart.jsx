57ec5d8e84a0d4827ddd08635a11c24c7fb3c050
Aditi Verma <aditiverma1009@gmail.com>
2021-03-09 21:24:07 +0530

    add post call

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,46 @@
 import React, { Component } from "react";
 import "./Cart.scss";
+import axios from "axios";
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+    };
+  }
+
+  componentDidMount = () => {
+    let total = 0;
+    this.props.cart.forEach((eachCartItem) => {
+      total += eachCartItem.inCartCount * eachCartItem.price;
+    });
+    this.setState({
+      total,
+    });
+  };
+
+  createOrder = () => {
+    axios.post(
+      "/orders",
+      {
+        items: this.props.cart.map((eachCartItem) => {
+          return {
+            id: eachCartItem.id,
+            name: eachCartItem.name,
+            price: eachCartItem.price,
+            count: eachCartItem.inCartCount,
+            category: eachCartItem.category,
+          };
+        }),
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   render() {
     if (this.props.cart.length > 0) {
       return (
@@ -23,6 +63,10 @@ export default class Cart extends Component {
               );
             })}
           </table>
+          <div className="total-value">
+            <div>total: {this.state.total}</div>
+            <button onClick={() => this.createOrder()}>Checkout</button>
+          </div>
         </div>
       );
     } else {
